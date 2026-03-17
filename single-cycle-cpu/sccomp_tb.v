0b538d0f342bab8dedfce85a9b6e3725dc7af17c
Echo-Kang-hub <3262955816@qq.com>
2026-03-17 09:47:30 +0800

    chore(single-cycle-cpu): 更新测试用例和结果文件
    更新了测试指令文件从Test_8_Instr.dat改为riscv-studentnosorting.dat，
    同时调整了测试结束条件的计数器阈值从1000到2000，
    并相应更新了PC比较地址从0x00000048到0x00000094，
    以匹配新的测试程序执行流程。

--- single-cycle-cpu/sccomp_tb.v
+++ single-cycle-cpu/sccomp_tb.v
@@ -15,7 +15,7 @@ module sccomp_tb();
   	integer counter = 0;
    
    initial begin
-      $readmemh( "Test_8_Instr.dat" , U_SCCOMP.U_IM.ROM); // load instructions into instruction memory
+      $readmemh( "riscv-studentnosorting.dat" , U_SCCOMP.U_IM.ROM); // load instructions into instruction memory
       $monitor("PC = 0x%8X, instr = 0x%8X", U_SCCOMP.PC, U_SCCOMP.instr); // used for debug
       foutput = $fopen("results.txt");
       clk = 1;
@@ -32,12 +32,12 @@ module sccomp_tb();
       #(50) clk = ~clk;
       
       if (clk == 1'b1) begin
-        if ((counter == 1000) || (U_SCCOMP.U_SCPU.PC_out=== 32'hxxxxxxxx)) begin
+        if ((counter == 2000) || (U_SCCOMP.U_SCPU.PC_out=== 32'hxxxxxxxx)) begin
           $fclose(foutput);
           $stop;
         end
         else begin
-          if (U_SCCOMP.PC == 32'h00000048) begin
+          if (U_SCCOMP.PC == 32'h00000094) begin
             counter = counter + 1;
             $fdisplay(foutput, "pc:\t %h", U_SCCOMP.PC);
             $fdisplay(foutput, "instr:\t\t %h", U_SCCOMP.instr);
